72e68df06f2a16160ed01be8d9b9a93cc91003ce
Sai Kumar Erumalla <saikumar.erumalla@gmail.com>
2025-08-29 14:49:31 +0900

    Web search Bot integrated

---
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -0,0 +1,60 @@
+from langgraph.graph import StateGraph
+from src.langgraphagenticai.state.state import State;
+from langgraph.graph import START, END
+from src.langgraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
+from src.langgraphagenticai.tools.search_tool import get_tools, create_tool_node
+from langgraph.prebuilt import ToolNode, tools_condition
+from src.langgraphagenticai.nodes.chatbot_with_tools_node import ChatbotWithToolsNode
+
+
+class GraphBuilder:
+    def __init__(self, model):
+        self.llm = model
+        self.graph_builder = StateGraph(State)
+
+    def basic_chatbot_graph_builder(self):
+        """
+        Builds a basic chatbot graph using LangGraph.
+        This method initializes a chatbot node using the `BasicChatbotNode` class 
+        and integrates it into the graph. The chatbot node is set as both the 
+        entry and exit point of the graph.
+        """
+        self.basic_chatbot_node=BasicChatbotNode(self.llm)
+
+        self.graph_builder.add_node("chatbot", self.basic_chatbot_node.process)
+        self.graph_builder.add_edge(START, "chatbot")
+        self.graph_builder.add_edge("chatbot", END)
+
+    def basic_chatbot_with_websearch_graph_builder(self):
+        """
+        Builds an advanced chatbot graph with tool integration.
+        This method creates a chatbot graph that includes both a chatbot node 
+        and a tool node. It defines tools, initializes the chatbot with tool 
+        capabilities, and sets up conditional and direct edges between nodes. 
+        The chatbot node is set as the entry point.
+        """
+        tools = get_tools()
+        tool_node = create_tool_node(tools)
+
+        llm = self.llm
+        obj_chatbot_with_tools_node = ChatbotWithToolsNode(llm)
+        chatbot_with_tools_node = obj_chatbot_with_tools_node.create_chatbot(tools)
+
+        self.graph_builder.add_node("chatbot", chatbot_with_tools_node)  
+        self.graph_builder.add_node("tools", tool_node)  
+        self.graph_builder.add_edge(START, "chatbot")
+        self.graph_builder.add_conditional_edges("chatbot",tools_condition)
+        self.graph_builder.add_edge("tools", "chatbot")
+
+
+
+    def setup_graph(self, usecase:str):
+        """
+        Sets up the graph for the selected use case.
+        """
+        if usecase == "Basic Chatbot":
+            self.basic_chatbot_graph_builder()
+        elif usecase == "Chatbot With WebSearch":
+            self.basic_chatbot_with_websearch_graph_builder()
+        
+        return self.graph_builder.compile()
---
+++ src/langgraphagenticai/main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from src.langgraphagenticai.UI.streamlitui.loadui import LoadStreamlitUi
+from src.langgraphagenticai.LLMs.groqllm import GroqLlm
+from src.langgraphagenticai.graph.graph_builder import GraphBuilder
+from src.langgraphagenticai.UI.streamlitui.display_result import DisplayResultStreamlit
+
+def load_langgraph_agentic_app():
+   """
+    Loads and runs the LangGraph AgenticAI application with Streamlit UI.
+    This function initializes the UI, handles user input, configures the LLM model,
+    sets up the graph based on the selected use case, and displays the output while 
+    implementing exception handling for robustness.
+
+    """
+
+   ui= LoadStreamlitUi()
+   user_input = ui.load_streamlit_ui()
+
+   if not user_input:
+       st.error("Please provide all the required inputs.")
+       return
+   user_message = st.chat_input("Enter your message here...")
+
+   if user_message:
+       try:
+           llm_config= GroqLlm(user_controls_input=user_input)
+           model = llm_config.get_llm_model()
+
+           if not model:
+                st.error("Failed to initialize the LLM model. Please check your configuration.")
+                return
+           usecase = user_input.get("selected_usecase")
+           if not usecase:
+                st.error("Please select a use case.")
+                return
+           graph_builder = GraphBuilder(model)
+           try:
+                graph = graph_builder.setup_graph(usecase)
+               #  print(user_message)
+                DisplayResultStreamlit(usecase, graph, user_message).display_result_on_ui()
+           except Exception as e:
+                st.error(f"Error setting up the graph: {e}")
+                return
+       except Exception as e:
+            st.error(f"Error initializing the LLM model: {e}")
+            return
